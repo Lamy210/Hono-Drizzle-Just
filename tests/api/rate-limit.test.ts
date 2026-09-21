ef1df18fe64cb91c903c46556b81f1a9d628ff53
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-21 13:53:30 +0900

    test: compose user listing in rate-limit app

--- tests/api/rate-limit.test.ts
+++ tests/api/rate-limit.test.ts
@@ -7,6 +7,7 @@ import { Sha256StringDigester } from "../../src/infrastructure/crypto/sha256-str
 import { JsonConsoleLogger } from "../../src/infrastructure/logging/json-console-logger";
 import { CreateUserService } from "../../src/modules/users/application/create-user.service";
 import { GetUserService } from "../../src/modules/users/application/get-user.service";
+import { ListUsersService } from "../../src/modules/users/application/list-users.service";
 import type { UserUnitOfWork } from "../../src/modules/users/application/user-unit-of-work";
 import type { UserRepository } from "../../src/modules/users/domain/user.repository";
 import { userUnitOfWork } from "../helpers/user-unit-of-work";
@@ -37,6 +38,9 @@ function buildApp(rateLimiter?: RateLimiter, remoteAddress?: string) {
         new Sha256StringDigester(),
       ),
       getUserService: new GetUserService(repository),
+      listUsersService: new ListUsersService({
+        listPage: mock(async () => ({ users: [], total: 0 })),
+      }),
       ...(rateLimiter === undefined ? {} : { rateLimiter }),
     },
     remoteAddress === undefined ? {} : { remoteAddressResolver: () => remoteAddress },
