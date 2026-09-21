502a3b7eebd56dc33045af14bdd73696b5a71663
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-21 13:53:08 +0900

    test: compose user listing in contract apps

--- scripts/openapi/document.ts
+++ scripts/openapi/document.ts
@@ -5,8 +5,10 @@ import { Sha256StringDigester } from "../../src/infrastructure/crypto/sha256-str
 import { JsonConsoleLogger } from "../../src/infrastructure/logging/json-console-logger";
 import { CreateUserService } from "../../src/modules/users/application/create-user.service";
 import { GetUserService } from "../../src/modules/users/application/get-user.service";
+import { ListUsersService } from "../../src/modules/users/application/list-users.service";
 import type { UserCreationIdempotencyRepository } from "../../src/modules/users/application/user-creation-idempotency.repository";
 import type { UserUnitOfWork } from "../../src/modules/users/application/user-unit-of-work";
+import type { UserListRepository } from "../../src/modules/users/application/user-list.repository";
 import type { UserRepository } from "../../src/modules/users/domain/user.repository";
 
 function buildContractApp() {
@@ -17,9 +19,10 @@ function buildContractApp() {
     name: "Contract User",
     createdAt: new Date("2026-09-16T00:00:00.000Z"),
   };
-  const repository: UserRepository = {
+  const repository: UserRepository & UserListRepository = {
     findById: async () => user,
     findByEmail: async () => null,
+    listPage: async () => ({ users: [user], total: 1 }),
     create: async (input) => ({ ...user, ...input }),
   };
   const idempotency: UserCreationIdempotencyRepository = {
@@ -44,6 +47,7 @@ function buildContractApp() {
       new Sha256StringDigester(),
     ),
     getUserService: new GetUserService(repository),
+    listUsersService: new ListUsersService(repository),
   });
 }
 
