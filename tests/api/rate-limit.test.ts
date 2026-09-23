9a63f5c4bbf41cfd0caf6432246675b9941b874d
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-24 01:47:29 +0900

    test: compose delete user dependency

--- tests/api/rate-limit.test.ts
+++ tests/api/rate-limit.test.ts
@@ -6,6 +6,7 @@ import type { TransactionManager } from "../../src/core/transaction/transaction-
 import { Sha256StringDigester } from "../../src/infrastructure/crypto/sha256-string-digester";
 import { JsonConsoleLogger } from "../../src/infrastructure/logging/json-console-logger";
 import { CreateUserService } from "../../src/modules/users/application/create-user.service";
+import { DeleteUserService } from "../../src/modules/users/application/delete-user.service";
 import { GetUserService } from "../../src/modules/users/application/get-user.service";
 import { ListUsersService } from "../../src/modules/users/application/list-users.service";
 import { UpdateUserService } from "../../src/modules/users/application/update-user.service";
@@ -33,6 +34,7 @@ function buildApp(rateLimiter?: RateLimiter, remoteAddress?: string) {
     {
       logger,
       readinessChecker: new ReadinessChecker([]),
+      deleteUserService: new DeleteUserService({ deleteById: mock(async () => false) }),
       createUserService: new CreateUserService(
         transactions(repository),
         logger,
